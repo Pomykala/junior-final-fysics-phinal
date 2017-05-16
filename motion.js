fb160ed39746b6d64093ad903314a34fb396cd2b
pomykala <8mpomykala@stpatrick.org>
2017-05-16 10:52:56 -0500

    Add files via upload
    motion is in motion

---
+++ motion.js
@@ -0,0 +1,32 @@
+var exports = module.exports = {};
+
+//use this prototype to create the other functions
+exports.distance = function(v,t){
+let d = v*t;
+  return d;
+};
+
+exports.distance = function(a,v,t){
+let d = 0.5*a*t*t+v*t;
+  return d;
+}
+exports.velocity(d,t){
+let v=d/t;
+  return v;
+}
+exports.velocity(d,a,t){
+let v=(d-0.5*a*a)/t;
+  return v;
+}
+exports.time(d,v){
+let t=d/v
+  return t
+}
+exports.time(){
+let (Vf)
+  return
+}
+exports.acceleration(){
+let
+  return
+}
